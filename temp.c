2fa909e505bf4be59cfdb1119dbbe05d324d9236
Niwedita17 <niwedita.dakshana2017@gmail.com>
2019-04-07 22:22:44 +0530

    lifo function

--- temp.c
+++ temp.c
@@ -7,7 +7,7 @@
 
 int main(int argc, char *argv[]) {
 	
-	float hit-ratio_srrip;
+	float hit-ratio_srrip,hit-ratio_lifo;
 	// ensure correct number of command line args
 	if (argc != 5) {
 		printf("usage: ./executable policy sets ways blocksize\n");
@@ -35,7 +35,94 @@ int main(int argc, char *argv[]) {
 	}
 	
 	hit-ratio_srrip=SRRIP(sets,ways);
+	hit-ratio_lifo=lifo(sets,ways);
 	return 0;
+}
+
+float lifo(long long sets,int ways)
+{
+   int ti=0;
+   struct data
+   {
+      long long tag;
+      int time;
+   }set[sets][ways];
+   int flag=0,i,j=0;
+   long long hit=0,miss=0,index=0;
+   int ways_filled[sets];
+   FILE* fptr = fopen("d3-16-final.txt", "r");
+   long long iter = 0;
+   for(; fscanf(fptr, "%lld", &iter) && !feof(fptr);)
+      {
+         if(flag==0)
+         {
+         index=iter;
+         flag=1;
+         }
+         else if(flag==1)
+         {
+            if(ways_filled[index]<ways)
+            {
+               for(i=0;i<ways_filled[index];i++)
+               {
+                  if(set[index][i].tag==iter)
+                  {
+                     hit++;
+                     set[index][i].time=ti;
+                     break;
+                  }
+               }
+               if((i==ways_filled[index] && ways_filled[index]!=0) || ways_filled[index]==0)
+               {
+                  miss++;
+                  set[index][ways_filled[index]].tag=iter;
+                  set[index][ways_filled[index]].time=ti;
+                  ways_filled[index]++;
+               }
+            }
+            else 
+            {
+               int maxti=0;
+               for(i=0;i<ways;i++)
+               {
+                  if(set[index][i].tag==iter)
+                  {
+                     hit++;
+                     set[index][i].time=ti;
+                     break;
+                  }
+               }
+               while(i==ways)
+               {
+                  miss++;
+                  for(i=0;i<ways;i++)
+                  {
+                     if(maxti<set[index][i].time)
+                     {
+                        maxti=set[index][i].time;
+                     }
+                  }
+                  for(i=0;i<ways;i++)
+                  {
+                     if(set[index][i].time==maxti)
+                     {
+                        set[index][i].tag=iter;
+                        set[index][i].time=ti;
+                        i=0;
+                        break;
+                     }
+                  }
+               }
+            }
+            flag=0;
+         }
+         ti++;
+      }
+   fclose(fptr);
+   float hitr;
+   hitr=(float)hit/(float)(hit+miss);
+   hitr=hitr*100;
+   return hitr;
 }
 
 float SRRIP(long long sets,int ways)
@@ -118,8 +205,7 @@ float SRRIP(long long sets,int ways)
             flag=0;
          }
       }
-   fclose(fptr);	
-	//printf("Hit-ratio=%f\tMiss-ratio=%f\n",((float)hit/(hit+miss)*100),((float)miss/(miss+hit)*100));
+   fclose(fptr);
 	return (float)(hit)/(hit+miss);
 }
 
