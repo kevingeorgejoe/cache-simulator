21438a5059ef176b3e4089937e96c24550adace0
Niwedita17 <niwedita.dakshana2017@gmail.com>
2019-04-07 22:17:32 +0530

    srrip function

--- temp.c
+++ temp.c
@@ -5,11 +5,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-void LRU(int sets, int ways, int blocksize);
-
 int main(int argc, char *argv[]) {
 	
+	float hit-ratio_srrip;
 	// ensure correct number of command line args
 	if (argc != 5) {
 		printf("usage: ./executable policy sets ways blocksize\n");
@@ -35,16 +33,93 @@ int main(int argc, char *argv[]) {
 		printf("call LRU()\n");
 		LRU(sets, ways, blocksize);
 	}
-
+	
+	hit-ratio_srrip=SRRIP(sets,ways);
 	return 0;
 }
 
-void LRU(int sets, int ways, int blocksize) {
-	// TODO
-	// read contents from file d1.txt and perform operations
-	// display relevant details required asked in .pdf file
-	
-	// repeat same process for d2.txt and d3.txt
-	printf("waddup this is LRU()\n");
+float SRRIP(long long sets,int ways)
+{
+   struct data
+   {
+      long long tag;
+      int valid;
+   }set[sets][ways];
 
+   // creating a FILE variable
+  int flag=0,i,j=0;
+  long long hit=0,miss=0,index=0;
+  int ways_filled[sets];
+
+  FILE* fptr = fopen("d3-16-final.txt", "r");
+   long long iter = 0;
+   for(; fscanf(fptr, "%lld", &iter) && !feof(fptr);)
+      {
+         if(flag==0)
+         {
+       //printf("index=%lld\n", iter);
+       index=iter;
+       flag=1;
+         }
+         else if(flag==1)
+         {
+            //printf("tag=%lld\n", iter);
+            if(ways_filled[index]<ways)
+            {
+		//printf("%d %d\n",i,ways_filled[index]);
+               for(i=0;i<ways_filled[index];i++)
+               {
+                  if(set[index][i].tag==iter)
+                  {
+                     hit++;
+                     set[index][i].valid=2;
+                     break;
+                  }
+               }
+               if((i==ways_filled[index] && ways_filled[index]!=0) || ways_filled[index]==0)
+               {
+                  miss++;
+                  set[index][ways_filled[index]].tag=iter;
+                  set[index][ways_filled[index]].valid=3;
+                  ways_filled[index]++;
+               }
+            }
+            else 
+            {
+               for(i=0;i<ways;i++)
+               {
+                  if(set[index][i].tag==iter)
+                  {
+                     hit++;
+                     set[index][i].valid=2;
+                     break;
+                  }
+               }
+               while(i==ways)
+               {
+                  miss++;
+                  for(i=0;i<ways;i++)
+                  {
+                     if(set[index][i].valid==3)
+                     {
+                        set[index][i].tag=iter;
+                        set[index][i].valid=0;
+                        i=0;
+                        break;
+                     }
+                  }
+                  if(i==ways)
+                  {
+                     for(j=0;j<ways;j++)
+                        set[index][j].valid++;
+                  }
+               }
+            }
+            flag=0;
+         }
+      }
+   fclose(fptr);	
+	//printf("Hit-ratio=%f\tMiss-ratio=%f\n",((float)hit/(hit+miss)*100),((float)miss/(miss+hit)*100));
+	return (float)(hit)/(hit+miss);
 }
+
